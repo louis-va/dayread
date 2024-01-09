e4c5de66f0fd40cdcf27bddfdf92c68364f6e46c
Alexander Fejari <alexander.fejari@gmail.com>
2024-01-09 11:21:58 +0100

    add features Typography

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import { ThemeProvider } from "./components/theme-provider";
 import "./main.css";
+import Typography from "@/features/Typography";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <main className="flex flex-col p-5 gap-20">
         <Nav />
         <section className="flex flex-col gap-8 items-center">
-          <h1>Shadcn is awesome</h1>
+          <Typography as={'h1'} className={'text-7xl underline'}>ShadCN is awesome</Typography>
         </section>
       </main>
     </ThemeProvider>
