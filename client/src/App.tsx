481db26919d118428e33b365adc2fb17c2e698d4
Alexander Fejari <alexander.fejari@gmail.com>
2024-01-09 15:11:20 +0100

    add layout general

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,14 @@ import Nav from "./components/Nav";
 import { ThemeProvider } from "./components/theme-provider";
 import "./main.css";
 import Typography from "@/features/Typography";
+import {Layout} from "@/features/Layout";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system">
-      <main className="flex flex-col p-5 gap-20">
-        <Nav />
-        <section className="flex flex-col gap-8 items-center">
-          <Typography as={'h1'} className={'text-7xl underline'}>ShadCN is awesome</Typography>
-        </section>
-      </main>
+        <Layout>
+            <Typography as={'h1'} className={'text-7xl underline'}>ShadCN is awesome</Typography>
+        </Layout>
     </ThemeProvider>
   );
 }
---
+++ client/src/features/Layout.tsx
@@ -0,0 +1,21 @@
+import Nav from "@/components/Nav";
+import {ReactNode} from "react";
+
+interface LayoutProps {
+    children?: ReactNode;
+}
+
+export const Layout = ({children}: LayoutProps) => {
+    return (
+        <section className="w-full h-dvh flex flex-col items-center">
+            <section className={"w-full h-full md:w-[64%] flex flex-col gap-4"}>
+                <Nav/>
+                <main className="flex">
+                    {children}
+                </main>
+                {/* A modifier par le bon component */}
+                <Nav/>
+            </section>
+        </section>
+    );
+};
