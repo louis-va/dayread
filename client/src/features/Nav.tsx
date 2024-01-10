08dfa0fdec5fb68750d8d5476cb67199fb2d3a79
bSchutters <bryan021@hotmail.be>
2024-01-10 15:29:15 +0100

    fix btn nav + react router

--- client/src/features/Nav.tsx
+++ client/src/features/Nav.tsx
@@ -6,10 +6,11 @@ import Icon from "../components/ui/icons";
 import { Button } from "../components/ui/button";
 import Logo from "@/components/ui/logo";
 import { Link } from "react-router-dom";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Nav() {
   const navDesktopClasses =
-    "md:flex md:flex-col md:relative md:justify-between md:gap-5 md:border-none py-5 md:py-0 md:h-full";
+    "md:flex md:flex-col md:relative md:justify-between md:gap-5 md:border-none py-5 md:py-0 md:h-full md:bg-background";
   const navMobileClasses =
     "fixed bottom-0 bg-popover/70 backdrop-blur-lg  border-border border-t-2 w-full z-10";
   return (
@@ -19,7 +20,12 @@ export default function Nav() {
           <Logo size={50} />
         </Link>
 
-        <Link to="/" className="flex gap-3">
+        <Link
+          to="/"
+          className={
+            buttonVariants({ variant: "ghost", size: "default" }) + "flex gap-3"
+          }
+        >
           <Icon icon="home" size={24} />
           <Typography className="hidden md:block">Home</Typography>
         </Link>
