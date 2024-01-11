cc224e152b3abf0fbb090ad6b01076838dc74300
Alexander-fejari <alexander.fejari@gmail.com>
2024-01-11 12:18:59 +0100

    avatars with avvvvatars libs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "./components/ui/theme-provider";
 import Typography from "./features/Typography";
+import Avatars from "@/features/Avatars.tsx";
 import "./main.css";
 
 import { Layout } from "@/features/Layout";
@@ -9,6 +10,7 @@ function App() {
     <ThemeProvider defaultTheme="system">
       <Layout>
         <Typography>Hello World</Typography>
+          <Avatars multiAvatars={true} icon={false} value={'test@gmail.com'} style={'shape'} border={true} borderSize={2} borderColor={'#27272A'}/>
       </Layout>
     </ThemeProvider>
   );
---
+++ client/src/features/Avatars.tsx
@@ -0,0 +1,62 @@
+import {ReactNode} from "react";
+import Avvvatars from 'avvvatars-react';
+import {Button} from "@/components/ui/button";
+import {PlusIcon} from '@radix-ui/react-icons';
+
+interface AvatarsProps {
+    children?: ReactNode;
+    value: string;
+    displayValue?: string;
+    style?: 'character' | 'shape';
+    size?: number;
+    shadow?: boolean;
+    radius?: number;
+    border?: boolean;
+    borderSize?: number;
+    borderColor?: string;
+    icon?: boolean;
+    multiAvatars?: boolean;
+}
+
+const Avatars = ({children, icon, multiAvatars, ...props}: AvatarsProps) => {
+    if (!icon && !multiAvatars) {
+        return (
+            <section>
+                <Avvvatars {...props}>
+                    {children}
+                </Avvvatars>
+            </section>
+        );
+    }
+
+    return (
+        <section>
+            {multiAvatars && (
+                <section className={'flex flex-row items-center w-fit relative'}>
+                    <Avvvatars {...props}>
+                        {children}
+                    </Avvvatars>
+                    <section className="absolute top-0 left-5">
+                        <Avvvatars {...props}>
+                            {children}
+                        </Avvvatars>
+                    </section>
+                </section>
+
+            )}
+            {icon && (
+                <section className="relative">
+                    <Avvvatars {...props}>
+                        {children}
+                    </Avvvatars>
+                    <Button className="absolute z-20 top-4 left-5 w-6 h-6 rounded-full border-2 border-border"
+                            size="icon">
+                        <PlusIcon className="text-[7px] stroke-2"/>
+                    </Button>
+                </section>
+            )}
+        </section>
+    );
+}
+
+export default Avatars;
