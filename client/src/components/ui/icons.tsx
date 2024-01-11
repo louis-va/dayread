83b4636165d7edb5d62b3d09d07c261a1323c111
bSchutters <bryan021@hotmail.be>
2024-01-11 18:16:35 +0100

    add posts feature

--- client/src/components/ui/icons.tsx
+++ client/src/components/ui/icons.tsx
@@ -2,27 +2,39 @@ import {
   LuHeart,
   LuHome,
   LuMenu,
+  LuMessageCircle,
   LuMoreHorizontal,
   LuPenSquare,
+  LuRepeat,
+  LuSend,
   LuUser,
 } from "react-icons/lu";
 
 interface IconProps {
-  icon: "home" | "like" | "newPost" | "profile" | "more" | "burger";
-  size?: 24 | 32 | 64;
+  icon:
+    | "home"
+    | "like"
+    | "newPost"
+    | "profile"
+    | "more"
+    | "burger"
+    | "retweet"
+    | "message"
+    | "send";
+  size?: 22 | 32 | 64;
   notif?: boolean;
-  className?: string;
+  className?: undefined | string;
 }
 
 export default function Icon({
   icon,
-  size = 24,
+  size = 22,
   notif = false,
   className,
 }: IconProps) {
   switch (size) {
-    case 24:
-      size = 24;
+    case 22:
+      size = 22;
       break;
     case 32:
       size = 32;
@@ -35,13 +47,59 @@ export default function Icon({
   if (notif === false) {
     return (
       <div className="icons">
-        {/* {icon === "home" && <LuHome size={size} className={className} />} */}
-        {icon === "home" && <LuHome size={size} className={className} />}
-        {icon === "like" && <LuHeart size={size} />}
-        {icon === "newPost" && <LuPenSquare size={size} />}
-        {icon === "profile" && <LuUser size={size} />}
-        {icon === "burger" && <LuMenu size={size} />}
-        {icon === "more" && <LuMoreHorizontal size={size} />}
+        {icon === "home" && (
+          <div className={className}>
+            <LuHome size={size} />
+          </div>
+        )}
+        {icon === "like" && (
+          <div
+            className={`${className} p-2 rounded-full flex items-center justify-center hover:bg-card ease-in-out duration-300`}
+          >
+            <LuHeart size={size} />
+          </div>
+        )}
+        {icon === "newPost" && (
+          <div className={className}>
+            <LuPenSquare size={size} />
+          </div>
+        )}
+        {icon === "profile" && (
+          <div className={className}>
+            <LuUser size={size} />
+          </div>
+        )}
+        {icon === "burger" && (
+          <div className={className}>
+            <LuMenu size={size} />
+          </div>
+        )}
+        {icon === "more" && (
+          <div className={className}>
+            <LuMoreHorizontal size={size} />
+          </div>
+        )}
+        {icon === "retweet" && (
+          <div
+            className={`${className} p-2 rounded-full flex items-center justify-center hover:bg-card ease-in-out duration-300`}
+          >
+            <LuRepeat size={size} />
+          </div>
+        )}
+        {icon === "message" && (
+          <div
+            className={`${className} p-2 rounded-full flex items-center justify-center hover:bg-card ease-in-out duration-300`}
+          >
+            <LuMessageCircle size={size} />
+          </div>
+        )}
+        {icon === "send" && (
+          <div
+            className={`${className} p-2 rounded-full flex items-center justify-center hover:bg-card ease-in-out duration-300`}
+          >
+            <LuSend size={size} />
+          </div>
+        )}
       </div>
     );
   }
