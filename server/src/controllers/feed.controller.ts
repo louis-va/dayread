705347e33be7d09e4e53a5e35cc2e6dcad81df15
Louis Van Aken <vanakenlouis@gmail.com>
2024-01-17 19:43:24 +0100

    feat(server): add get discovery feed endpoint

--- server/src/controllers/feed.controller.ts
+++ server/src/controllers/feed.controller.ts
@@ -13,12 +13,12 @@ async function getFeed(req: Request, res: Response) {
     const followingList = await Follow.find({ follower: user.id }).select('following');
     const followingIds = followingList.map((follow) => follow.following);
 
-    // Fetch the 10 most recent posts from followers with pagination
+    // Fetch the 10 most recent posts from followees with pagination
     const page = parseInt(req.query.page as string) || 1;
     const pageSize = 10;
     const skip = (page - 1) * pageSize;
 
-    const recentPosts = await Post.find({ author: { $in: followingIds }, is_comment: false })
+    const recentPosts = await Post.find({ author: { $in: followingIds, $ne: user.id }, is_comment: false })
       .sort({ created_date: -1 })
       .skip(skip)
       .limit(pageSize)
@@ -34,4 +34,33 @@ async function getFeed(req: Request, res: Response) {
   }
 }
 
-export default { getFeed }
+async function getDiscoveryFeed(req: Request, res: Response) {
+  try {
+    const user = req.user as IUser
+
+    // Get the users that the current user is following
+    const followingList = await Follow.find({ follower: user.id }).select('following');
+    const followingIds = followingList.map((follow) => follow.following);
+
+    // Fetch the 10 most recent posts from non-followees with pagination
+    const page = parseInt(req.query.page as string) || 1;
+    const pageSize = 10;
+    const skip = (page - 1) * pageSize;
+
+    const recentPosts = await Post.find({ author: { $nin: followingIds, $ne: user.id }, is_comment: false })
+      .sort({ created_date: -1 })
+      .skip(skip)
+      .limit(pageSize)
+      .populate('author', 'username')
+      .exec();
+
+    // Count number of likes and comments
+    const postsDetails = await calculateLikesAndComments(recentPosts)
+
+    return res.status(200).send(postsDetails);
+  } catch (err: any) {
+    return res.status(500).send({ message: err });
+  }
+}
+
+export default { getFeed, getDiscoveryFeed }
