b90be8cb2599584e9b45bdec23d181d301d4a2f6
bSchutters <bryan021@hotmail.be>
2024-01-18 15:16:12 +0100

    fix Home

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,6 +1,5 @@
 import NewPost from "@/features/NewPost";
 import { Layout } from "@/features/Layout";
-
 import { Toaster } from "@/components/ui/toaster";
 import Feed from "@/features/feed/feed";
 import {
