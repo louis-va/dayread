b32cf85192a297d7d53673fbfa06b61f9963f397
bSchutters <bryan021@hotmail.be>
2024-01-18 15:19:55 +0100

    fix Home v2

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -15,7 +15,6 @@ function Home() {
     <Layout>
       <Toaster />
       <NewPost />
-
       <TabsHome defaultValue="forYou" className="w-full">
         <TabsListHome>
           <TabsTriggerHome value="forYou">Pour vous</TabsTriggerHome>
